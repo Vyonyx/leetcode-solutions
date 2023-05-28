d7b13c7346c42409ad70ca706272936bce804bb3
personal <shaneel_kumar@live.com>
2023-05-28 13:41:48 +1000

    add a more performant solution for valid anagram algorithm (TS)

--- easy-solutions/valid-anagram.ts
+++ easy-solutions/valid-anagram.ts
@@ -1,3 +1,6 @@
+/*
+ * The commented code is my first solution and is less performant.
+
  function isAnagram(s: string, t: string): boolean {
     if (s.length !== t.length) return false;
 
@@ -5,4 +8,32 @@
     t = t.toLowerCase().split('').sort().join('');
 
     return s == t;
+};
+*/
+
+type HashMap = {
+    [key: string]: number;
+}
+
+const isAnagram = (s: string, t: string): boolean => {
+    if (s.length !== t.length) return false;
+    const hashmap: HashMap = {}
+
+    for (let ch of s) {
+        if (hashmap[ch]) {
+            hashmap[ch]++;
+        } else {
+            hashmap[ch] = 1;
+        }
+    }
+
+    for (let ch of t) {
+        if (hashmap[ch]) {
+            hashmap[ch]--;
+        } else {
+            return false;
+        }
+    }
+
+    return true;
 };
