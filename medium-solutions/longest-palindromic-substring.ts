23c182fb6a5abb1ade33e0c1c2f7c564c1a8d474
personal <shaneel_kumar@live.com>
2023-06-19 20:57:49 +1000

    add solution for longest palindromic substring algorithm (TS)

---
+++ medium-solutions/longest-palindromic-substring.ts
@@ -0,0 +1,27 @@
+function longestPalindrome(s: string): string {
+    if (s.length === 1) return s.substring(0, 1);
+
+    let longest = '';
+
+    for (let i = 0; i < s.length; i++) {
+        [expand(s, i, i), expand(s, i, i + 1)]
+            .forEach(str => {
+                if (str.length > longest.length)
+                    longest = str;
+            })
+    }
+
+    return longest;
+};
+
+function expand(s: string, left: number, right: number) {
+    while (
+        right < s.length
+        && s[left] === s[right]
+    ) {
+        left--;
+        right++;
+    }
+
+    return s.substring(left + 1, right);
+}
